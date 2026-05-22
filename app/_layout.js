01c4a61c8e3e5cbfbd7bedaef7c7bccb2fcb3f85
seopchang <yunseobchang123@gmail.com>
2026-05-22 20:35:25 +0900

    fix: disable new arch, add splash screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,15 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { LanguageProvider } from '../src/context/LanguageContext';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
   return (
     <LanguageProvider>
       <Stack screenOptions={{ headerShown: false }} />
